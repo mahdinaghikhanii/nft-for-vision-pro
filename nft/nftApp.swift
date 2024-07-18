ce5a9150f0577a81a557813ac4e6ab2c563d02c1
Mahdinkh7 <mahdinkh7@gmail.com>
2024-07-18 23:37:49 +0330

    first commit

---
+++ nft/nftApp.swift
@@ -0,0 +1,17 @@
+//
+//  nftApp.swift
+//  nft
+//
+//  Created by mahdi naghikhani on 18/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct nftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
